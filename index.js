0b90912d031d386edfb0a84095d440b7e3216c11
Jack210797 <suvorov21079@gmail.com>
2024-05-08 17:26:52 +0300

    class-carousel.1

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import SwipeCarousel from './swipe.js'
 const carousel = new SwipeCarousel({
   containerId: '#carousel',
   slideId: '.slide',
-  interval: 1000,
-  isPlaying: false,
+  interval: 2000,
+  isPlaying: true,
 })
   
 carousel.init()
